112388372a57cc3b9435adb164ddee3c8ddc20e5
maxdml <maxdemoulin@gmail.com>
2015-04-16 15:04:47 -0400

    redirect outputs to /local/init.log

--- puppet.sh
+++ puppet.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
-yum install -y puppet-server puppet
-chkconfig puppetmaster on
-HN="`hostname`"
-sed -i "s/#PUPPET_SERVER=puppet/PUPPET_SERVER='$HN'/; s/#PUPPET_LOG/PUPPET_LOG/" /etc/sysconfig/puppet
-sed -i '14 c    dns_alt_names = puppet,'$HN /etc/puppet/puppet.conf
-wget http://www.cs.duke.edu/~maxdml/experiments-setter/spark-master.pp
-puppet apply spark-master.pp 
+rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm &>> /local/init.log 
+yum install -y puppet-server puppet &>> /local/init.log
+chkconfig puppetmaster on &>> /local/init.log
+HN="`hostname`" &>> /local/init.log
+sed -i "s/#PUPPET_SERVER=puppet/PUPPET_SERVER='$HN'/; s/#PUPPET_LOG/PUPPET_LOG/" /etc/sysconfig/puppet &>> /local/init.log
+sed -i '14 c    dns_alt_names = puppet,'$HN /etc/puppet/puppet.conf &>> /local/init.log 
+wget http://www.cs.duke.edu/~maxdml/experiments-setter/spark-master.pp &>> /local/init.log
+puppet apply spark-master.pp  &>> /local/init.log
