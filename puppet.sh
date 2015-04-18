74468bca9703ed6dc4805c1fc02a2fa976fd229c
maxdml <maxdemoulin@gmail.com>
2015-04-18 12:40:41 -0400

    now download guests-nodes.pp and apply it

--- puppet.sh
+++ puppet.sh
@@ -8,5 +8,7 @@ sed -i "s/#PUPPET_SERVER=puppet/PUPPET_SERVER='$HN'/; s/#PUPPET_LOG/PUPPET_LOG/"
 sed -i '14 c    dns_alt_names = puppet,'$HN /etc/puppet/puppet.conf
 wget http://www.cs.duke.edu/~maxdml/experiments-setter/master-node.pp
 wget http://www.cs.duke.edu/~maxdml/experiments-setter/spark-master.pp
+wget http://www.cs.duke.edu/~maxdml/experiments-setter/guests-nodes.pp
 puppet apply master-node.pp
 puppet apply spark-master.pp
+puppet apply guests-nodes.pp
